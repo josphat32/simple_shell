4f781ecd9e4a67eb07137f282493cd934a99d069
Josphat32 <mwangijosphat2030@gmail.com>
2022-11-16 03:12:43 -0800

    builtin that pass the command in the arg

--- get_builtin.c
+++ get_builtin.c
@@ -1,7 +1,7 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * get_builtin - builtin that pais the command in the arg
+ * get_builtin - builtin that pass the command in the arg
  * @cmd: command
  * Return: function pointer of the builtin command
  */
